10092633f3b44dcae450e94dc5919b26a3fd59a6
ruben bras <ru3enbras@gmail.com>
2024-10-28 11:45:29 +0000

    base de dados

---
+++ basedados/basedados.h
@@ -0,0 +1,18 @@
+<?php
+
+$database ='felixBus';
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(! $conn ){
+die('Could not connect: ' . mysqli_error($conn));
+}
+
+mysqli_select_db($conn , $database);
+
+$retval = mysqli_query($conn , $sql);//linha para correr a querry do sql
+
+
+?>
