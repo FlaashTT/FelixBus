d86b80afa6320be3fa896b0f214c96ab73b51d84
ruben bras <ru3enbras@gmail.com>
2024-11-10 19:09:27 +0000

    update

--- paginas/login.php
+++ paginas/login.php
@@ -1,1 +1,14 @@
-ola
+
+
+<?php
+if($_POST["username"] !="" && $_POST["password"] !=""){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    echo("bem vindo $username <br>");
+    echo("pass $password");
+}else{
+    echo("Preencha todos os campos!");
+}
+
+?>
