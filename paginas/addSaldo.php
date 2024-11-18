51f163187c6a11234936b2bb1c78435045cc2e1b
ruben bras <ru3enbras@gmail.com>
2024-11-18 11:20:39 +0000

    update

---
+++ paginas/addSaldo.php
@@ -0,0 +1,26 @@
+<?php
+$opcao = $_GET["addSaldo"];
+
+switch ($opcao) {
+    case 0:
+        echo "O ",$eleitor," votou em branco <br>";
+    break;
+    case 1:
+        echo "O ",$eleitor," ,votou em <br>";
+        
+        for($i=0;$i<=$NumOpcoes;$i++){
+            $selected = $opcao[$i];
+        }
+        
+    break;
+    case 2:
+        echo "O ",$eleitor," votou em branco <br>";
+    break;
+    case 3:
+        echo "O ",$eleitor," tem o seu voto anulado <br>";
+    break;
+    case 4:
+        echo "O ",$eleitor,", tem seu voto seus voto anulado! <br>";
+    break;
+    }
+?>
