d312388ce3e9e85212f364c78477a2c857e4065a
ruben bras <ru3enbras@gmail.com>
2025-02-25 17:48:12 +0000

    bd feita

--- basedados/criar_bd.sql
+++ basedados/criar_bd.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2025 at 05:05 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Tempo de geração: 25-Fev-2025 às 18:46
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `felixbusrecurso`
+-- Banco de dados: `felixbusrecurso`
 --
 CREATE DATABASE IF NOT EXISTS `felixbusrecurso` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `felixbusrecurso`;
@@ -26,7 +26,7 @@ USE `felixbusrecurso`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `alertas`
+-- Estrutura da tabela `alertas`
 --
 
 CREATE TABLE IF NOT EXISTS `alertas` (
@@ -37,10 +37,10 @@ CREATE TABLE IF NOT EXISTS `alertas` (
   `Tipo_Alerta` enum('Novo Registo','Promoção','Aviso','Criar Veículo','Editar Veículo','Eliminar Veículo','Aceitar Utilizador','Editar Utilizador','Eliminar Utilizador','Eliminar Rota','Criar Rota','Editar Rota','Editar Bilhete','Cancelar Bilhete','Criar Bilhete','Rejeitar Utilizador','Compra','Reembolso') NOT NULL,
   PRIMARY KEY (`id`),
   KEY `Id_Remetente` (`Id_Remetente`)
-) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `alertas`
+-- Extraindo dados da tabela `alertas`
 --
 
 INSERT INTO `alertas` (`id`, `Texto_Alerta`, `Data_Emissao`, `Id_Remetente`, `Tipo_Alerta`) VALUES
@@ -49,12 +49,16 @@ INSERT INTO `alertas` (`id`, `Texto_Alerta`, `Data_Emissao`, `Id_Remetente`, `Ti
 (5, 'Novo utilizador registado', '2025-02-07 19:06:51', 5, 'Novo Registo'),
 (97, 'Criou um novo veículo com matrícula: A12', '2025-02-22 14:27:17', 3, 'Criar Veículo'),
 (98, 'Criou uma nova rota: Viagem', '2025-02-22 14:27:44', 3, 'Criar Rota'),
-(99, 'Bilhete criado para a rota 6', '2025-02-22 14:28:24', 3, 'Criar Bilhete');
+(99, 'Bilhete criado para a rota 6', '2025-02-22 14:28:24', 3, 'Criar Bilhete'),
+(102, 'Criou um novo veículo com matrícula: zx-70-ab', '2025-02-25 17:44:18', 3, 'Criar Veículo'),
+(103, 'Criou uma nova rota: CBlb', '2025-02-25 17:44:50', 3, 'Criar Rota'),
+(104, 'Bilhete criado para a rota 7', '2025-02-25 17:45:23', 3, 'Criar Bilhete'),
+(105, 'Promoção na rota ID 7: Desconto de 50% em bilhetes.', '2025-02-25 17:45:56', 3, 'Promoção');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bilhetes`
+-- Estrutura da tabela `bilhetes`
 --
 
 CREATE TABLE IF NOT EXISTS `bilhetes` (
@@ -70,19 +74,20 @@ CREATE TABLE IF NOT EXISTS `bilhetes` (
   PRIMARY KEY (`id_bilhete`),
   KEY `id_rota` (`id_rota`),
   KEY `id_veiculo` (`id_veiculo`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `bilhetes`
+-- Extraindo dados da tabela `bilhetes`
 --
 
 INSERT INTO `bilhetes` (`id_bilhete`, `id_rota`, `id_veiculo`, `preco`, `data`, `hora`, `estado_bilhete`, `lugaresComprados`, `data_criacao`) VALUES
-(26, 6, 5, 50.00, '2025-07-31', '18:32:00', 'Ativo', 0, '2025-02-22 14:28:24');
+(26, 6, 5, 50.00, '2025-07-31', '18:32:00', 'Ativo', 0, '2025-02-22 14:28:24'),
+(27, 7, 6, 250.00, '2025-08-20', '20:50:00', 'Ativo', 0, '2025-02-25 17:45:23');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `compras_bilhetes`
+-- Estrutura da tabela `compras_bilhetes`
 --
 
 CREATE TABLE IF NOT EXISTS `compras_bilhetes` (
@@ -99,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `compras_bilhetes` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rota`
+-- Estrutura da tabela `rota`
 --
 
 CREATE TABLE IF NOT EXISTS `rota` (
@@ -109,19 +114,20 @@ CREATE TABLE IF NOT EXISTS `rota` (
   `Destino` varchar(255) NOT NULL,
   `Distancia` decimal(10,2) NOT NULL,
   PRIMARY KEY (`Id_Rota`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `rota`
+-- Extraindo dados da tabela `rota`
 --
 
 INSERT INTO `rota` (`Id_Rota`, `Nome_Rota`, `Origem`, `Destino`, `Distancia`) VALUES
-(6, 'Viagem', 'Castelo Branco', 'Covilhã', 50.00);
+(6, 'Viagem', 'Castelo Branco', 'Covilhã', 50.00),
+(7, 'CBlb', 'Castelo Branco', 'Lisboa', 250.00);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilizadores`
+-- Estrutura da tabela `utilizadores`
 --
 
 CREATE TABLE IF NOT EXISTS `utilizadores` (
@@ -139,18 +145,18 @@ CREATE TABLE IF NOT EXISTS `utilizadores` (
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `utilizadores`
+-- Extraindo dados da tabela `utilizadores`
 --
 
 INSERT INTO `utilizadores` (`id`, `Nome`, `Email`, `Password`, `Cargo`, `Autenticacao`, `Saldo`, `Estado`, `data_registro`) VALUES
-(3, 'admin', 'admin@gmail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Admin', 'Aceite', 0.00, 'Offline', '2025-02-07 18:06:04'),
+(3, 'admin', 'admin@gmail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Admin', 'Aceite', 0.00, 'Online', '2025-02-07 18:06:04'),
 (4, 'funcionario', 'funcionario@gmail.com', '24d96a103e8552cb162117e5b94b1ead596b9c0a94f73bc47f7d244d279cacf2', 'Funcionario', 'Aceite', 0.00, 'Offline', '2025-02-07 18:06:28'),
 (5, 'cliente', 'cliente@gmail.com', 'a60b85d409a01d46023f90741e01b79543a3cb1ba048eaefbe5d7a63638043bf', 'Cliente', 'Aceite', 0.00, 'Offline', '2025-02-07 18:06:51');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `veiculos`
+-- Estrutura da tabela `veiculos`
 --
 
 CREATE TABLE IF NOT EXISTS `veiculos` (
@@ -160,34 +166,35 @@ CREATE TABLE IF NOT EXISTS `veiculos` (
   `Matricula` varchar(20) NOT NULL,
   PRIMARY KEY (`Id_Veiculo`),
   UNIQUE KEY `Matricula` (`Matricula`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `veiculos`
+-- Extraindo dados da tabela `veiculos`
 --
 
 INSERT INTO `veiculos` (`Id_Veiculo`, `Nome_Veiculo`, `Capacidade`, `Matricula`) VALUES
-(5, 'FelixBus', 50, 'A12');
+(5, 'FelixBus', 50, 'A12'),
+(6, 'mercedes A40', 40, 'zx-70-ab');
 
 --
--- Constraints for dumped tables
+-- Restrições para despejos de tabelas
 --
 
 --
--- Constraints for table `alertas`
+-- Limitadores para a tabela `alertas`
 --
 ALTER TABLE `alertas`
   ADD CONSTRAINT `alertas_ibfk_1` FOREIGN KEY (`Id_Remetente`) REFERENCES `utilizadores` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `bilhetes`
+-- Limitadores para a tabela `bilhetes`
 --
 ALTER TABLE `bilhetes`
   ADD CONSTRAINT `bilhetes_ibfk_1` FOREIGN KEY (`id_rota`) REFERENCES `rota` (`Id_Rota`) ON DELETE CASCADE,
   ADD CONSTRAINT `bilhetes_ibfk_2` FOREIGN KEY (`id_veiculo`) REFERENCES `veiculos` (`Id_Veiculo`) ON DELETE CASCADE;
 
 --
--- Constraints for table `compras_bilhetes`
+-- Limitadores para a tabela `compras_bilhetes`
 --
 ALTER TABLE `compras_bilhetes`
   ADD CONSTRAINT `compras_bilhetes_ibfk_1` FOREIGN KEY (`id_bilhete`) REFERENCES `bilhetes` (`id_bilhete`),
